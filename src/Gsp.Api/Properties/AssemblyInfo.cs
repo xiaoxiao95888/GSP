df0e157b48b0c5716b7222b4fbab1c1499ef5639
lock0 <xiaoxiao95888@163.com>
2017-01-25 10:32:02 +0800

    add api

--- src/Gsp.Api/Properties/AssemblyInfo.cs
+++ src/Gsp.Api/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的常规信息通过下列特性集
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("Gsp.Web")]
+[assembly: AssemblyTitle("Gsp.Api")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Gsp.Web")]
+[assembly: AssemblyProduct("Gsp.Api")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于 typelib 的 ID
-[assembly: Guid("b0136e51-dc8d-473e-9045-0f146875f468")]
+[assembly: Guid("34ad843c-86a9-4a16-92f2-1a022fa9891a")]
 
 // 程序集的版本信息由下列四个值组成:
 //
--- src/Gsp.Web/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Gsp.Web.WebApiApplication" Language="C#" %>
